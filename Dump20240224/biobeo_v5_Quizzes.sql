7be3da26d5843bc10e9c54c2c89d63df635e2c99
mohamed khattab <mkhattab369@gmail.com>
2024-02-25 13:59:22 +0200

    changes

--- Dump20240224/biobeo_v5_Quizzes.sql
+++ Dump20240224/biobeo_v5_Quizzes.sql
@@ -1,40 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
---
--- Host: database-1.cvaq82ci2yxb.eu-north-1.rds.amazonaws.com    Database: biobeo_v5
--- ------------------------------------------------------
--- Server version	8.0.35
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
-
---
--- GTID state at the beginning of the backup 
---
-
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
-
---
--- Table structure for table `Quizzes`
---
-
-DROP TABLE IF EXISTS `Quizzes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-
-
--- ========================================
--- Quiz is a question
--- ========================================
 CREATE TABLE `Quizzes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `poi_id` int DEFAULT NULL,
@@ -48,17 +12,4 @@ CREATE TABLE `Quizzes` (
   KEY `poi_id` (`poi_id`),
   CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`poi_id`) REFERENCES `POIs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `quizzes_chk_1` CHECK ((`correct_option` in (_utf8mb4'A',_utf8mb4'B',_utf8mb4'C')))
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-02-24 19:52:43
+) 
