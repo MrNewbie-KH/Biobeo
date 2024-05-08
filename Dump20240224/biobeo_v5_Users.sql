8b75cbe782247ad98d7c2ec1f71b5202af4350f9
TulipG <137293132+TulipG@users.noreply.github.com>
2024-05-08 13:37:40 +0100

    Update biobeo_v6_Users.sql

--- Dump20240224/biobeo_v5_Users.sql
+++ Dump20240224/biobeo_v5_Users.sql
@@ -6,7 +6,7 @@ CREATE TABLE `Users` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `role` enum('teacher','student') NOT NULL DEFAULT 'student',
   `points` int DEFAULT NULL,
-  `photo_URL` varchar int DEFAULT NULL,
+  `photo_URL` varchar(255) DEFAULT NULL,
   `completed_routes` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
